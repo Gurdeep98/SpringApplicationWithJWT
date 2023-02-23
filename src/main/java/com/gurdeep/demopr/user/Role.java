25da1b6295a640b4deeb7e2e19df9f3e178f0175
Gurdeep Prajapati <42078328+Gurdeep98@users.noreply.github.com>
2023-02-23 20:41:42 +0530

    initial commit

---
+++ src/main/java/com/gurdeep/demopr/user/Role.java
@@ -0,0 +1,6 @@
+package com.gurdeep.demopr.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/gurdeep/demopr/DemoprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurdeep.demopr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
